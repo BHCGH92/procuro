25c289865bc6cd66af17d328be7d70708cb97df8
BHCGH92 <ben.holt1992@live.co.uk>
2025-12-12 22:55:25 +0000

    Data Model for Suppliers has been created and a sleeker admin view for it created. Migrations ran for suppliers.

--- suppliers/admin.py
+++ suppliers/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Supplier, PaymentMethod
 
 # Register your models here.
+
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = (
+        'supplier_name', 
+        'contact_number', 
+        'email', 
+        'supplier_status', 
+        'payment_method'
+    )
+    search_fields = (
+        'supplier_name', 
+        'email', 
+        'main_contact'
+    )
+    list_filter = (
+        'supplier_status', 
+        'payment_method'
+    )
+    list_editable = (
+        'supplier_status',
+    )
+    ordering = ('supplier_name',)
+admin.site.register(Supplier, SupplierAdmin)
--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class PaymentMethod(models.TextChoices):
+    BACS = 'BACS', 'BACS'
+    BANK_TRANSFER = 'BANK TRANSFER', 'Bank Transfer'
+    CASH = 'CASH', 'Cash'
+    CHEQUE = 'CHEQUE', 'Cheque'
+
+class Supplier(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    supplier_name = models.CharField(max_length=250, unique=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    contact_number = models.CharField(max_length=20, blank=True, null=True)
+    supplier_status = models.BooleanField(default=True)
+    main_contact = models.CharField(max_length=250, blank=True, null=True)
+    payment_method = models.CharField(max_length=15, choices=PaymentMethod.choices, default=PaymentMethod.BANK_TRANSFER)
